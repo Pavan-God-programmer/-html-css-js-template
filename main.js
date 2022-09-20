a045ee568e8883db4c66c4508ea6c47bec57fb70
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-20 11:06:20 -0600

    deone

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // JavaScript Code
+
+//input
+let number1 = +prompt("Enter Client-Side Scripting 1 Grade:");
+let number2 = +prompt("Enter Structured Programming 1 Grade:");
+let number3 = +prompt("Enter Structured Programming 2 Grade:");
+let number4 = +prompt("Enter Client-Side Scripting 2 Grade:");
+let number5 = +prompt("Enter Project A Grade:");
+
+
+//Process
+let total =  ( (number1 + number2 + number3 + number4 + number5) / 5);
+
+//Output
+alert(`Your Average is : ${total}%`);  
