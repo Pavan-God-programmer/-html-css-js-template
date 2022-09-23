badf7164b661871ee3f4e69d77fcb182040520ea
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-23 11:20:58 -0600

    wowowoowooww

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 // JavaScript Code
 
 //input
-let number1 = +prompt("Enter Client-Side Scripting 1 Grade:");
-let number2 = +prompt("Enter Structured Programming 1 Grade:");
-let number3 = +prompt("Enter Structured Programming 2 Grade:");
-let number4 = +prompt("Enter Client-Side Scripting 2 Grade:");
-let number5 = +prompt("Enter Project A Grade:");
-
-
 //Process
-let total =  ( (number1 + number2 + number3 + number4 + number5) / 5);
+const button = document.getElementById("btn1");
 
+button.addEventListener("click", () => {
+  let number1 = parseInt(document.getElementById("q1").value || "0");
+  let number2 = parseInt(document.getElementById("q2").value || "0");
+  let number3 = parseInt(document.getElementById("q3").value || "0");
+  let number4 = parseInt(document.getElementById("q4").value || "0");
+  let number5 = parseInt(document.getElementById("q5").value || "0");
+
+  let total = (number1 + number2 + number3 + number4 + number5) / 5;
+
+  const resElem = document.getElementById("result1");
+
+  resElem.innerHTML = `Final CS10 Grades: ${total}%`;
+
+  if (total < 50) {
+    document.getElementById("msg").innerHTML = "Better Luck Next Time Kid!😢";
+  }
+});
 //Output
-alert(`Your Average is : ${total}%`);  
