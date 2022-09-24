ee3199db7fb289864fa5b46937f95191b7dcecb6
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-23 20:26:39 -0600

    done

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 // JavaScript Code
 
-//input
-//Process
-const button = document.getElementById("btn1");
+//Event Listerner on the button 
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-button.addEventListener("click", () => {
-  let number1 = parseInt(document.getElementById("q1").value || "0");
-  let number2 = parseInt(document.getElementById("q2").value || "0");
-  let number3 = parseInt(document.getElementById("q3").value || "0");
-  let number4 = parseInt(document.getElementById("q4").value || "0");
-  let number5 = parseInt(document.getElementById("q5").value || "0");
 
-  let total = (number1 + number2 + number3 + number4 + number5) / 5;
+function btnClicked() {
+   //input
+  let number1 = +document.getElementById("q1").value;
+  let number2 = +document.getElementById("q2").value;
+  let number3 = +document.getElementById("q3").value;
+  let number4 = +document.getElementById("q4").value;
+  let number5 = +document.getElementById("q5").value; 
+ 
+   // Process 
+    let total = (number1 + number2 + number3 + number4 + number5) / 5;
 
-  const resElem = document.getElementById("result1");
+  // Output
+  document.getElementById("output").innerHTML = total;
+  
+}
 
-  resElem.innerHTML = `Final CS10 Grades: ${total}%`;
-
-  if (total < 50) {
-    document.getElementById("msg").innerHTML = "Better Luck Next Time Kid!😢";
-  }
-});
-//Output
