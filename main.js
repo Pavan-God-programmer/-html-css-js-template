cd90fc3acc2e6875114c325d7b25e00d26aa0c2b
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-26 10:32:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 // JavaScript Code
 
-//Event Listerner on the button 
+//Event Listerner on the button
 document.getElementById("btn").addEventListener("click", btnClicked);
 
-
 function btnClicked() {
-   //input
+  //input
   let number1 = +document.getElementById("q1").value;
   let number2 = +document.getElementById("q2").value;
   let number3 = +document.getElementById("q3").value;
   let number4 = +document.getElementById("q4").value;
-  let number5 = +document.getElementById("q5").value; 
- 
-   // Process 
-    let total = (number1 + number2 + number3 + number4 + number5) / 5;
+  let number5 = +document.getElementById("q5").value;
+
+  // Process
+  let total = (number1 + number2 + number3 + number4 + number5) / 5;
 
   // Output
   document.getElementById("output").innerHTML = total;
-  
 }
-
